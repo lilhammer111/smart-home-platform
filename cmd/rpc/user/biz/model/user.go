65a5aa7504fd3e0cfeff645893dfa69720663731
lilhammer111 <wwwwwdemon@gmail.com>
2023-12-19 22:25:17 +0800

    modified the user model

--- cmd/rpc/user/biz/model/user.go
+++ cmd/rpc/user/biz/model/user.go
@@ -3,22 +3,19 @@ package model
 import (
 	"errors"
 	"gorm.io/gorm"
-	"time"
 )
 
 type User struct {
 	BaseModel
-	Username string    `gorm:"type:varchar(30);index" json:"username"`
-	Password string    `gorm:"type:varchar(255)" json:"password"`
-	Openid   string    `gorm:"type:varchar(50)" json:"openid"` // current length is 28
-	Age      int8      `gorm:"type:tinyint unsigned" json:"age"`
-	Gender   int8      `gorm:"type:tinyint unsigned" json:"gender"`
-	Email    string    `gorm:"type:varchar(100);index" json:"email"` // varchar(254) RFC5321 STANDARD
-	Mobile   string    `gorm:"type:varchar(11);index" json:"mobile"`
-	Profile  string    `gorm:"type:varchar(255)" json:"profile"`
-	Avatar   string    `gorm:"type:varchar(200)" json:"avatar"`
-	IsFrozen bool      `gorm:"type:bool;comment:whether user account frozen, 1 means user is frozen,0 for normal" json:"is_frozen"`
-	ThawedAt time.Time `gorm:"type:datetime;comment:time when user accounts unfrozen" json:"thawed_at"`
+	Username string `gorm:"type:varchar(30);index;unique" json:"username"`
+	Password string `gorm:"type:varchar(255)" json:"password"`
+	Openid   string `gorm:"type:varchar(50);index;unique" json:"openid"` // current length is 28
+	Age      int8   `gorm:"type:tinyint unsigned" json:"age"`
+	Gender   int8   `gorm:"type:tinyint unsigned" json:"gender"`
+	Email    string `gorm:"type:varchar(100);index;unique" json:"email"` // varchar(254) RFC5321 STANDARD
+	Mobile   string `gorm:"type:varchar(11);index;unique" json:"mobile"`
+	Profile  string `gorm:"type:varchar(255)" json:"profile"`
+	Avatar   string `gorm:"type:varchar(200)" json:"avatar"`
 }
 
 // BeforeDelete is a delete hook
